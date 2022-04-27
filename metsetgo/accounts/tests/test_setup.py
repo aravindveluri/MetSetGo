14ea93e953dfe7e2260e22307fe90ba671e14818
rohitgarg2411 <rohitg2024@gmail.com>
2022-04-27 23:28:24 +0530

    tests added for accounts

---
+++ metsetgo/accounts/tests/test_setup.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+# from authentication.models import User
+
+
+class TestSetUp(APITestCase):
+
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.user_url = reverse('user')
+
+        self.user_data = {
+            'fname':'a',
+            'email': 'ab@ab.com',
+            'username': "ab",
+            'password': "ab",
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
--- metsetgo/accounts/urls.py
+++ metsetgo/accounts/urls.py
@@ -3,9 +3,9 @@ from .api import SignUpAPI, SignInAPI, MainUser
 from knox import views as knox_views
 
 urlpatterns = [
-    path('api/auth/', include('knox.urls'),name='knox-auth'),
-    path('api/auth/register', SignUpAPI.as_view(),name="knox-register"),
-    path('api/auth/login', SignInAPI.as_view(),name="knox-login"),
-    path('api/auth/user', MainUser.as_view(),name="knox-user"),
-    path('api/auth/logout',knox_views.LogoutView.as_view(), name="knox-logout"),
+    path('api/auth/', include('knox.urls'),name='auth'),
+    path('api/auth/register', SignUpAPI.as_view(),name="register"),
+    path('api/auth/login', SignInAPI.as_view(),name="login"),
+    path('api/auth/user', MainUser.as_view(),name="user"),
+    path('api/auth/logout',knox_views.LogoutView.as_view(), name="logout"),
 ]
