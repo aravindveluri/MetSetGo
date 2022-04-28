2e4966c9317d9b898b9a2d9c2a0f853d5e26b4d0
aravindveluri <aravind.veluri@iiitb.org>
2022-04-28 19:13:25 +0530

    Config settings for GitHub Actions

--- metsetgo/metsetgo/settings.py
+++ metsetgo/metsetgo/settings.py
@@ -85,16 +85,29 @@ WSGI_APPLICATION = 'metsetgo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': str(os.getenv('DBNAME')),
-        'USER': str(os.getenv('DBUSER')),
-        'PASSWORD': str(os.getenv('DBPASSWORD')),
-        'HOST': str(os.getenv('DBHOST')),
-        'PORT': str(os.getenv('DBPORT')),
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': str(os.getenv('DBNAME')),
+            'USER': str(os.getenv('DBUSER')),
+            'PASSWORD': str(os.getenv('DBPASSWORD')),
+            'HOST': str(os.getenv('DBHOST')),
+            'PORT': str(os.getenv('DBPORT')),
+        }
+    }
+
 
 
 # Password validation
