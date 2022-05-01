9473a424db771d50f6da95f59d9852046609576c
aravindveluri <aravind.veluri@iiitb.org>
2022-05-01 22:00:13 +0530

    Dockerfiles & docker-compose configured

--- metsetgo/metsetgo/settings.py
+++ metsetgo/metsetgo/settings.py
@@ -14,12 +14,13 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+load_dotenv(os.path.join(str(Path(BASE_DIR).cwd()), '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
