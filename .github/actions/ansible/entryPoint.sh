5507e8bbf517d6f6c20e3bca1a92587634721cdc
arvi178 <43075004+aravindveluri@users.noreply.github.com>
2022-05-06 05:38:06 +0530

    Update entryPoint.sh

--- .github/actions/ansible/entryPoint.sh
+++ .github/actions/ansible/entryPoint.sh
@@ -2,7 +2,7 @@
 echo "Ansible Entrypoint"
 
 echo "[azure]" >> /hosts
-echo "20.205.22.244 ansible_connection=ssh ansible_user=$AZURE_SSH_USER ansible_ssh_user=$AZURE_SSH_USER ansible_python_interpreter=/usr/bin/python3 ansible_ssh_pass=$AZURE_SSH_PASSWORD ansible_become_pass=$AZURE_SSH_PASSWORD" >> /hosts
+echo "20.205.22.244 ansible_connection=ssh ansible_user=$AZURE_SSH_USER ansible_ssh_user=$AZURE_SSH_USER ansible_python_interpreter=/usr/bin/python3.8 ansible_ssh_pass=$AZURE_SSH_PASSWORD ansible_become_pass=$AZURE_SSH_PASSWORD" >> /hosts
 
 echo "Entering the ansible using ansible-playbook"
 
