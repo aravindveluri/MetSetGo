785d45c801da9862e5f0cf525bb434925c8f5196
arvi178 <43075004+aravindveluri@users.noreply.github.com>
2022-05-09 17:32:17 +0530

    Debug entrypoint.sh

--- .github/actions/ansible/entryPoint.sh
+++ .github/actions/ansible/entryPoint.sh
@@ -3,6 +3,12 @@ echo "Ansible Entrypoint"
 
 # Debug
 printenv
+echo "Files list"
+ls -al
+echo "Github workspace debug"
+ls /github/workspace
+
+
 
 echo "[azure]" >> /hosts
 echo "20.205.22.244 ansible_connection=ssh ansible_user=$AZURE_SSH_USER ansible_ssh_user=$AZURE_SSH_USER ansible_python_interpreter=/usr/bin/python3.8 ansible_ssh_pass=$AZURE_SSH_PASSWORD ansible_become_pass=$AZURE_SSH_PASSWORD" >> /hosts
